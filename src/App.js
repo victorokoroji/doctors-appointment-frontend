ef81a3a5a4aa910164aff0d10480933f4a798a0c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-07-21 11:21:10 +0000

    Restructure the sign up form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterForm from './components/Register';
-// import Sidebar from './components/Sidebar';
 import Login from './components/Login';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-      {/* <Sidebar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterForm />} />
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,34 +3,44 @@ import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const SignupForm = () => {
-  // const initialValues = { name: '', email: '', password: '' };
-  // const [formValues, setFormValues] = useState(initialValues);
-  // const [formErrors, setFormErrors] = useState({});
-  // const [isSubmit, setIsSubmit] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
-  const passwordConfirmRef = useRef(null);
+  const passwordConfirmRef = useRef();
 
   useEffect(() => {
     nameRef.current.focus();
-  }, []);
+
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log('Operation successful'); // eslint-disable-line no-console
+    }
+  }, [errors]);
+
+  const validate = () => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!regex.test(emailRef.current.value)) {
+      errors.message = 'This is not a valid email format!';
+    }
+    if (passwordRef.current.value.length < 6 || passwordRef.current.value.length > 40) {
+      errors.message = 'The password must be between 6 and 40 characters';
+    }
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      errors.message = 'The passwords do not match!';
+    }
+    return errors;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setError('');
-
-    if (!nameRef.current.value || !emailRef.current.value || !passwordRef.current.value) {
-      setError('Please fill out all the fields');
-    }
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      setError('The passwords do not match!');
-    }
-    return setError();
+    setErrors(validate);
+    setIsSubmit(true);
   };
 
   return (
@@ -41,10 +51,14 @@ const SignupForm = () => {
           <hr />
         </div>
         <div className="errors">
-          <p style={{ color: 'red' }}>{error}</p>
+          {Object.keys(errors).length === 0 && isSubmit ? (
+            <div className="text-success mb-4 text-center">Account created successfully</div>
+          ) : (
+            <p className="text-danger mb-4">{errors.message}</p>
+          )}
         </div>
         <div className="form-container">
-          <form onSubmit={(e) => handleSubmit(e)}>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <input
                 type="text"
@@ -81,9 +95,12 @@ const SignupForm = () => {
               <label htmlFor="password" className="input-label">Password</label>
               <button
                 type="button"
+                style={{ border: 'none', outline: 'none', background: '#fff' }}
                 onClick={() => setShowPassword(!showPassword)}
               >
-                {showPassword ? <FaEye className="eye-icon" /> : <FaEyeSlash className="eye-icon" />}
+                {showPassword
+                  ? <FaEye className="eye-icon" />
+                  : <FaEyeSlash className="eye-icon" />}
               </button>
 
             </div>
@@ -101,6 +118,7 @@ const SignupForm = () => {
               </label>
               <button
                 type="button"
+                style={{ border: 'none', outline: 'none', background: '#fff' }}
                 onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
               >
                 {showPasswordConfirmation
@@ -116,7 +134,7 @@ const SignupForm = () => {
         <div>
           <p>
             Already have an account?
-            <Link to="/login">Login</Link>
+            <Link to="/login" style={{ color: '#44522e', textDecoration: 'none' }}> Login</Link>
           </p>
         </div>
       </div>
@@ -125,112 +143,3 @@ const SignupForm = () => {
 };
 
 export default SignupForm;
-
-/** const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
-
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues); // eslint-disable-line no-console
-    }
-  }, [formErrors]);
-
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.name) {
-      errors.name = 'Name is required!';
-    } else if (values.name.length < 3 || values.name.length > 20) {
-      errors.name = 'The name must be between 3 and 20 characters';
-    }
-    if (!values.email) {
-      errors.email = 'Email is required!';
-    } else if (!regex.test(values.email)) {
-      errors.email = 'This is not a valid email format!';
-    }
-    if (!values.password) {
-      errors.password = 'Password is required';
-    } else if (values.password.length < 6 || values.password.length > 40) {
-      errors.password = 'The password must be between 6 and 40 characters';
-    }
-    return errors;
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
-  };
-
-  return (
-    <div className="container">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="text-center text-success mb-5">Account created successfully</div>
-      ) : (
-        null
-      )}
-
-      <form
-        style={{ width: '32%', background: '#f7f7f7' }}
-        className="mx-auto border border-2 p-5 mt-5 rounded"
-        onSubmit={handleSubmit}
-        autoComplete="off"
-      >
-        <img
-          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-          alt="register-img"
-          className="rounded-circle mx-auto d-block mb-4"
-          style={{ width: '35%', height: '35%' }}
-        />
-        <div className="mb-3">
-          <>{ //eslint-disable-next-line jsx-a11y/label-has-associated-control //}</>
-          <label htmlFor="nameInput" className="form-label">Name</label>
-          <input
-            type="text"
-            id="nameInput"
-            className="form-control"
-            placeholder="Enter name"
-            name="name"
-            value={formValues.name}
-            onChange={handleChange}
-          />
-          <p className="text-danger">{formErrors.name}</p>
-        </div>
-        <div className="mb-3">
-          <>{ //eslint-disable-next-line jsx-a11y/label-has-associated-control // }</>
-          <label htmlFor="emailInput" className="form-label">Email</label>
-          <input
-            type="email"
-            id="emailInput"
-            className="form-control"
-            placeholder="Enter email"
-            name="email"
-            value={formValues.email}
-            onChange={handleChange}
-          />
-          <p className="text-danger">{formErrors.email}</p>
-        </div>
-        <div className="mb-3">
-          <>{ // eslint-disable-next-line jsx-a11y/label-has-associated-control // }</>
-          <label htmlFor="passwordInput" className="form-label">Password</label>
-          <input
-            type="password"
-            in="passwordinput"
-            className="form-control"
-            placeholder="Enter password"
-            name="password"
-            value={formValues.password}
-            onChange={handleChange}
-          />
-          <p className="text-danger">{formErrors.password}</p>
-        </div>
-        <button type="submit" className="btn btn-primary">Register</button>
-      </form>
-    </div>
-  );
-*/
