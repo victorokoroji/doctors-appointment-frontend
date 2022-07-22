93cd5b743dc0947c4524eaa1d886ff8d267cabda
iLynette <lynetteacholah@icloud.com>
2022-07-22 13:19:24 +0300

    Get appointments

--- src/redux/appointments/appointments.js
+++ src/redux/appointments/appointments.js
@@ -3,3 +3,15 @@ import userServices from '../services/userServices';
 const FETCH_APPOINTMENTS = 'DOCTORS-APPOINTMENT/FETCH_APPOINTMENTS';
 const CREATE_APPOINTMENTS = 'DOCTORS-APPOINTMENT/CREATE_APPOINTMENTS';
 const DELETE_APPOINTMENTS = 'DOCTORS-APPOINTMENT/DELETE_APPOINTMENTS';
+
+const initialState = [];
+
+export const getAppointments = () => async (dispatch) => {
+  const result = await userServices.getAllAppointments();
+  return dispatch({
+    type: FETCH_APPOINTMENTS,
+    payload: result,
+  });
+};
+
+
