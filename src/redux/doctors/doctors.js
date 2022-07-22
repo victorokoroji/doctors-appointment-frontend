56ceb68f59a709ce90557b8975196bae00286724
iLynette <lynetteacholah@icloud.com>
2022-07-22 16:55:12 +0300

    Get doctors

--- src/redux/doctors/doctors.js
+++ src/redux/doctors/doctors.js
@@ -4,3 +4,12 @@ const FETCH_DOCTORS = 'DOCTORS-APPOINTMENT/FETCH_DOCTORS';
 const CREATE_DOCTORS = 'DOCTORS-APPOINTMENT/CREATE_DOCTORS';
 const DELETE_DOCTORS = 'DOCTORS-APPOINTMENT/DELETE_DOCTORS';
 
+const initialState = { doctors: [] };
+
+export const getDoctors = () => async (dispatch) => {
+  const result = await userServices.getDoctors();
+  return dispatch({
+    type: FETCH_DOCTORS,
+    payload: result,
+  });
+};
