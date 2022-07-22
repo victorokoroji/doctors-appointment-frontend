38d28e1f9ad93424623d1293d809b76bf27b67c1
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-22 03:01:16 -0700

    Add react icons

---
+++ src/components/ReserveForm.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import './css/reserveform.css';
+
+const ReserveForm = () => {
+  return (
+    <div className="body-container">
+        ReserveForm
+        
+    </div>
+  )
+}
+
+export default ReserveForm
