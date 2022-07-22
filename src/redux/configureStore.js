8abacd1ab717d917ca7c908ddf83da008066d025
iLynette <lynetteacholah@icloud.com>
2022-07-22 17:12:15 +0300

    Add reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const rootReducer = combineReducers({ /* reducers go here*/ });
+const rootReducer = combineReducers({ 
+    doctorsReducer,
+    appointmentReducer
+ });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
