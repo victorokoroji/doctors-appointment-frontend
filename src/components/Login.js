76c3c0d5bf2a2f09f9e58d7f1a0f38f99414ce3e
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-23 10:08:12 -0700

    add merge

--- src/components/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Login = () => (
-  <>
-    <h3 className="text-center">You can follow this link to login to your account!</h3>
-  </>
-);
-
-export default Login;
