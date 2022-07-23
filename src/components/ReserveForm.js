5a03d92877071c5716efd18857858acf3f255f70
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-23 06:39:00 -0700

    Refactor code to add css module

--- src/components/ReserveForm.js
+++ src/components/ReserveForm.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import style from './css/reserveform.module.css';
 import { FaBars } from 'react-icons/fa';
+import style from './css/reserveform.module.css';
 
 const ReserveForm = () => (
-  <section className="style.reserveContainer">
-    <div className="style.mainBody">
-      <div className="style.sideButton">
-        <button type="button" className="style.hamburger">
+  <section className={style.reserveContainer}>
+    <div className={style.mainBody}>
+      <div className={style.sideButton}>
+        <button type="button" className={style.hamburger}>
           <FaBars />
         </button>
       </div>
-      <div className="style.heading">
-        <h4 className="style.reserveFormHeading"> Book Appointment </h4>
+      <div className={style.heading}>
+        <h4 className={style.reserveFormHeading}> Book Appointment </h4>
       </div>
-      <div className="style.bodyText">
-        <p className="style.paragraph">
+      <div className={style.bodyText}>
+        <p className={style.paragraph}>
           We have different doctors from different parts of the world.
           <br />
           If you wish to find out if we have a doctor who is available
           in your city, please use the selector below
         </p>
       </div>
-      <div className="style.formBody">
-        <div className="style.formBlock lg:block">
+      <div className={style.formBody}>
+        <div className={style.formBlock}>
           <input
             type="text"
             placeholder="city"
             value="city"
-            className="style.formInput"
+            className={style.formInput}
           />
 
           <select
             name="availableDoctors"
             id="availableDoctors"
-            className="style.selectDoctors"
+            className={style.selectDoctors}
           >
             <option value="Faith"> Choose a Doctor </option>
             <option value="Faith"> Dr.Faith </option>
@@ -44,17 +44,17 @@ const ReserveForm = () => (
           <input
             type="date"
             value="dd/mm/yy"
-            className="style.inputDate"
+            className={style.inputDate}
           />
           <br />
         </div>
 
       </div>
-      <div className="style.buttonBody">
+      <div className={style.buttonBody}>
         <input
           type="submit"
           value="Book Now"
-          className="style.bookButton"
+          className={style.bookButton}
         />
       </div>
     </div>
