d435792ad560d350cfbbffef066ed5aa426d14f5
iLynette <lynetteacholah@icloud.com>
2022-07-27 15:54:14 +0300

    Action and initial state

---
+++ src/redux/register/register.js
@@ -0,0 +1,11 @@
+import userServices from '../services/userServices';
+
+const REGISTER_API = 'DOCTORS-APPOINTMENT/REGISTER_API';
+
+const initialState = {
+  isLoggedIn: false,
+  user: {},
+  status: '',
+};
+
+
