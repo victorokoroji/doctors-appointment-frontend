624e8804b9f3594b33fbb83e3bc035b5addf84d5
iLynette <lynetteacholah@icloud.com>
2022-07-27 15:49:50 +0300

    Login reducer in store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import doctorsReducer from './doctors/doctors';
 import appointmentReducer from './appointments/appointments';
+import loginReducer from './login/login';
+import registerReducer from './register/register';
 
 const rootReducer = combineReducers({
   doctorsReducer,
   appointmentReducer,
+  loginReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
