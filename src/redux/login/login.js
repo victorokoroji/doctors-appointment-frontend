06a8c665627b0a52eed26d9fa3029c1734768e4e
iLynette <lynetteacholah@icloud.com>
2022-07-27 15:46:56 +0300

    Set up login actions

---
+++ src/redux/login/login.js
@@ -0,0 +1,7 @@
+import userServices from '../services/userServices';
+
+const LOGIN_API = 'DOCTORS-APPOINTMENT/LOGIN_API';
+const LOGOUT_API = 'DOCTORS-APPOINTMENT/LOGOUT_API';
+
+export const IS_LOGIN = 'DOCTORS-APPOINTMENT/IS_LOGIN';
+
