547f1b5b65fabdd24f29166807493a12b044e285
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-28 11:44:05 -0700

    Add logic for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,31 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable consistent-return */
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, shallowEqual, useSelector } from 'react-redux';
 import style from '../css/login.module.css';
 import { isLoggedIn } from '../redux/login/login';
 
 const LoginForm = () => {
-  const dispatch = useDispatch();
   const initialValues = { name: '', password: '' };
-  const [formValues, setFormValues] = useState(initialValues);
+  const [user, setUser] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
+  const nameRef = useRef();
+  const passwordRef = useRef();
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({
-      ...formValues,
+      ...user,
       [name]: value,
     });
   };
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      setFormErrors(formValues);
-    }
-  }, [formErrors]);
+
+  const myData = useSelector((state) => state.loginReducer, shallowEqual);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const validate = (values) => {
     const errors = {};
@@ -39,19 +41,39 @@ const LoginForm = () => {
     }
     return errors;
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      const userData = { formValues };
+      dispatch(isLoggedIn(userData));
+
+      console.log(userData);
+      console.log(myData);
+
+      setTimeout(() => {
+        navigate('/doctors');
+      }, 3000);
+    }
+
     setIsSubmit(true);
   };
   return (
     <section className={style.sessionForm}>
       <div className={style.sessionContainer}>
-        {Object.keys(formErrors).length === 0 && isSubmit ? (
+        {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
           <div className="text-center text-success mb-5">Account created successfully</div>
-        ) : null}
+        ) : null} */}
+
+        {myData.status === 200 && isSubmit ? (
+          <div className={style.success}>Account created successfully</div>
+        ) : (
+          <p className={style.errorMsg}>{formErrors.message}</p>
+        )}
+
         <div className={style.formContainer}>
-          <form onSubmit={handleSubmit} autoComplete="off" className={style.form}>
+          <form onSubmit={(e) => handleSubmit(e)} autoComplete="off" className={style.form}>
             <div className={style.heading}>
               <h1>Login</h1>
               <hr className={style.line} />
@@ -60,6 +82,7 @@ const LoginForm = () => {
               <input
                 type="text"
                 id={style.emailInput}
+                ref={nameRef}
                 className={style.inputField}
                 name="name"
                 value={formValues.name}
@@ -77,6 +100,7 @@ const LoginForm = () => {
                 type="password"
                 id="passwordinput"
                 className={style.inputField}
+                ref={passwordRef}
                 name="password"
                 value={formValues.password}
                 onChange={handleChange}
@@ -90,7 +114,7 @@ const LoginForm = () => {
             <div className={style.submitBtn}>
               <button
                 type="submit"
-                onClick={() => dispatch(isLoggedIn(formValues))}
+                // onClick={() => dispatch(isLoggedIn(formValues))}
               >
                 Login
               </button>
